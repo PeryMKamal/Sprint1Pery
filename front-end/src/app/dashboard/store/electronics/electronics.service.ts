ee74120a4de0893cfab887f3bae9e8f825394712
PeryMKamal <perihan.hussien@student.guc.edu.eg>
2018-03-02 20:59:50 +0200

    i changed in electronics

---
+++ front-end/src/app/dashboard/store/electronics/electronics.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+
+@Injectable()
+export class ElectronicsService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  createProduct(name:string, price:number, component:string, seller:string) {
+    return this.httpClient.post(environment.apiUrl + 'perihan/createProduct', {'name':name, 'price':price , 'component':component,'seller':seller});
+  }
+
+   getProducts(){
+     return this.httpClient.get(environment.apiUrl + 'perihan/getProducts');
+   }
+
+   updateProduct(id:object, name:string, price:number) {
+    return this.httpClient.patch(environment.apiUrl + 'perihan/updateProduct/'+id,{'name':name, 'price':price});
+  }
+
+  deleteProduct(id:object){
+    return this.httpClient.delete(environment.apiUrl+ 'perihan/deleteProduct/'+id);
+  }
+
+}
